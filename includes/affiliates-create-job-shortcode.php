3b7ece1aed6008049d8f1aebe63894bc9e68e47d
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-21 18:42:32 +0000

    Patched some HTML issues

--- includes/affiliates-create-job-shortcode.php
+++ includes/affiliates-create-job-shortcode.php
@@ -12,15 +12,15 @@ function affiliates_create_job_widget() {
         <h3>Create a Job</h3>
         <form id="affiliates-create-job-form">
             <label for="job-title">Job Title:</label>
-            <input type="text" id="job-title" name="title" required>
+            <input type="text" id="job-title" name="title" required/>
             
             <label for="job-description">Job Description:</label>
-            <textarea id="job-description" name="description" required>
+            <textarea id="job-description" name="description" required></textarea>
             
             <label for="job-contact">Contact:</label>
-            <input type="email" id="job-contact" name="contact" required>
+            <input type="email" id="job-contact" name="contact" required/>
             
-            <input type="submit" value="Create Job">
+            <input type="submit" value="Create Job"/>
         </form>
         <div id="job-response"></div>
     </div>
