a5397efed087345274e9eff474acb13cccae741b
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-25 17:47:36 +0000

    Updated the logout logic

--- includes/affiliates-logout-shortcode.php
+++ includes/affiliates-logout-shortcode.php
@@ -1,6 +1,6 @@
 <?php
 function custom_logout_button() {
-    $logout_url = wp_logout_url('https://creol.ucf.edu');
+    $logout_url = 'https://creol.ucf.edu';
     return '<a href="' . esc_url($logout_url) . '" class="btn btn-primary">Logout</a>';
 }
 add_shortcode('logout_button', 'custom_logout_button');
