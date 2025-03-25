32e4d8b8d6e497b5e175009b402f863160ad826f
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-25 17:50:32 +0000

    Try to destroy current session

--- includes/affiliates-logout-shortcode.php
+++ includes/affiliates-logout-shortcode.php
@@ -1,7 +1,9 @@
 <?php
 function custom_logout_button() {
-    wp_clear_auth_cookie();
-    // wp_destroy_current_session();
+    if ( is_user_logged_in() ) {
+        wp_clear_auth_cookie();
+        wp_destroy_current_session();
+    }
     $logout_url = 'https://creol.ucf.edu';
     return '<a href="' . esc_url($logout_url) . '" class="btn btn-primary">Logout</a>';
 }
