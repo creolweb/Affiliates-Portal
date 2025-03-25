bb6dc1f580d4d52d7b99a6ed6d966e94ecb8381e
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-25 17:52:04 +0000

    Reset the current user

--- includes/affiliates-logout-shortcode.php
+++ includes/affiliates-logout-shortcode.php
@@ -3,6 +3,7 @@ function custom_logout_button() {
     if ( is_user_logged_in() ) {
         wp_clear_auth_cookie();
         wp_destroy_current_session();
+        wp_set_current_user( 0 );
     }
     $logout_url = 'https://creol.ucf.edu';
     return '<a href="' . esc_url($logout_url) . '" class="btn btn-primary">Logout</a>';
