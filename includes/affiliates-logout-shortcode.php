b091848ac52f331f6207fb5dade7c912cfd9fede
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-25 17:48:52 +0000

    Try to clear cookies before the redirect

--- includes/affiliates-logout-shortcode.php
+++ includes/affiliates-logout-shortcode.php
@@ -1,5 +1,7 @@
 <?php
 function custom_logout_button() {
+    wp_clear_auth_cookie();
+    // wp_destroy_current_session();
     $logout_url = 'https://creol.ucf.edu';
     return '<a href="' . esc_url($logout_url) . '" class="btn btn-primary">Logout</a>';
 }
