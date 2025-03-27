1ba8d5fd96c43e66492da5fffba042363840d669
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-27 18:46:48 +0000

    Reverted the changes made to the logout menu

--- affiliates-portal.php
+++ affiliates-portal.php
@@ -44,18 +44,3 @@ add_action('init', 'register_my_menus');
 function register_my_menus() { 
     register_nav_menu('affiliates-menu', __('Affiliates Menu')); 
 } 
-
-add_action('wp_logout', 'custom_logout_redirect');
-function custom_logout_redirect() {
-    wp_redirect('https://creol.ucf.edu'); // Change to your desired logout URL
-    exit();
-}
-
-function add_logout_link_to_menu( $items, $args ) {
-    if ( is_user_logged_in() && $args->theme_location === 'affiliates-menu' ) {
-        $logout_url = wp_logout_url();
-        $items .= '<li class="menu-item"><a href="' . esc_url( $logout_url ) . '">Logout</a></li>';
-    }
-    return $items;
-}
-add_filter( 'wp_nav_menu_items', 'add_logout_link_to_menu', 10, 2 );
