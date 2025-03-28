230217d8cb4f7a2a63b648337947b34026bf9069
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-03-28 20:53:53 +0000

    Adjusted margins

--- templates/affiliates-list-jobs.php
+++ templates/affiliates-list-jobs.php
@@ -1,4 +1,4 @@
 <div id="affiliates-portal-widget" data-is-self="<?php echo ( $is_self ? '1' : '0' ); ?>">
-    <h3>Job Listings</h3>
+    <h3 class="mb-4">Job Listings</h3>
     <div id="affiliates-job-list"></div>
 </div>
