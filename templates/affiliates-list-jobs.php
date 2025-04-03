4bd9abdec88c35d666f754cce8845704db0abfd9
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-04-03 21:30:45 +0000

    Adjusted the padding of list jobs widget

--- templates/affiliates-list-jobs.php
+++ templates/affiliates-list-jobs.php
@@ -1,4 +1,10 @@
-<div id="affiliates-portal-widget" data-is-self="<?php echo ( $is_self ? '1' : '0' ); ?>">
-    <h3 class="mb-4">Job Listings</h3>
-    <div id="affiliates-job-list"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div id="affiliates-portal-widget" data-is-self="<?php echo ( $is_self ? '1' : '0' ); ?>">
+                <h3 class="mb-4">Job Listings</h3>
+                <div id="affiliates-job-list"></div>
+            </div>
+        </div>
+    </div>
 </div>
